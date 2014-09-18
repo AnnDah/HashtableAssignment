5f0d8e9c8e20e3ba6865a1c12cc5af71394200dd
Annika Magnusson <annika.h.magnusson@gmail.com>
2014-09-18 11:29:37 +0200

    Added code from teacher

--- HashTableAssignment/Entry.cs
+++ HashTableAssignment/Entry.cs
@@ -8,5 +8,31 @@ namespace HashTableAssignment
 {
     class Entry
     {
+        private object key;
+
+        public object Key
+        {
+            get { return key; }
+            set { key = value; }
+        }
+        private object value;
+
+        public object Value
+        {
+            get { return this.value; }
+            set { this.value = value; }
+        }
+
+        public Entry(object key, object value)
+        {
+            this.key = key;
+            this.value = value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Entry keyValue = (Entry)obj;
+            return key.Equals(keyValue.key);
+        }
     }
 }
--- HashTableAssignment/Hashtable.cs
+++ HashTableAssignment/Hashtable.cs
@@ -8,5 +8,55 @@ namespace HashTableAssignment
 {
     class Hashtable
     {
+        private LinkedList<object> insertionOrder = new LinkedList<object>();
+        private LinkedList<Entry>[] table;
+
+        public Hashtable(int size)
+        {
+            table = new  LinkedList<Entry>[size];
+            for(int i = 0; i < size; i++)
+            {
+                table[i] = new LinkedList<Entry>();
+            }
+        }
+
+        private int HashIndex(object key)
+        {
+            int hashCode = key.GetHashCode();
+            hashCode = hashCode % table.Length;
+            return (hashCode < 0) ? -hashCode : hashCode;
+        }
+
+        public object Get(object key)
+        {
+            int hashIndex = HashIndex(key);
+            if (table[hashIndex].Contains(new Entry(key, null)))
+            {
+                Entry entry = table[hashIndex].Find(new Entry(key, null)).Value;
+                return entry.Value;
+            }
+
+            return null;
+        }
+
+        public int Count
+        {
+            get { return insertionOrder.Count(); }
+        }
+
+        public void Put(object key, object value)
+        {
+
+        }
+
+        public void Remove(object key)
+        {
+
+        }
+
+        public LinkedList<object> GetInsertionOrder()
+        {
+            return insertionOrder;
+        }
     }
 }
